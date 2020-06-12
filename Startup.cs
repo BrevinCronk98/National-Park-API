1c5e76527920cbb771b3ebc589e74a1e4738828e
brevin <cronkbrevin98@gmail.com>
2020-06-12 09:08:29 -0700

    Added CORS to the startup file and enabled it globally, any other domain can now use data from my api

--- Startup.cs
+++ Startup.cs
@@ -20,11 +20,22 @@ namespace NationalParkAPI
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins ="_myAllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {  
+                builder.WithOrigins("http://example.com",
+                                    "http://www.contoso.com");
+            });
+        });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,6 +51,7 @@ namespace NationalParkAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseHttpsRedirection();
             app.UseMvc();
