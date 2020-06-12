438cbd89ef1f12aeda13cfa9275ed9dff7c9887d
brevin <cronkbrevin98@gmail.com>
2020-06-12 14:56:32 -0700

    All 50 states are seeded, going to work on seeding join table

--- Models/ParkAPIContext.cs
+++ Models/ParkAPIContext.cs
@@ -130,247 +130,247 @@ namespace NationalParkAPI.Models
                             new State
                             {
                                 StateId = 2,
-                                StateName = "Alabama"
+                                StateName = "Alaska"
                             },
                             new State
                             {
                                 StateId = 3,
-                                StateName = "Alabama"
+                                StateName = "Arizona"
                             },
                             new State
                             {
                                 StateId = 4,
-                                StateName = "Alabama"
+                                StateName = "Arkansas"
                             },
                             new State
                             {
                                 StateId = 5,
-                                StateName = "Alabama"
+                                StateName = "California"
                             },
                             new State
                             {
                                 StateId = 6,
-                                StateName = "Alabama"
+                                StateName = "Colorado"
                             },
                             new State
                             {
                                 StateId = 7,
-                                StateName = "Alabama"
+                                StateName = "Conneticut"
                             },
                             new State
                             {
                                 StateId = 8,
-                                StateName = "Alabama"
+                                StateName = "Delaware"
                             },
                             new State
                             {
                                 StateId = 9,
-                                StateName = "Alabama"
+                                StateName = "Florida"
                             },
                             new State
                             {
                                 StateId = 10,
-                                StateName = "Alabama"
+                                StateName = "Georgia"
                             },
                             new State
                             {
                                 StateId = 11,
-                                StateName = "Alabama"
+                                StateName = "Hawaii"
                             },
                             new State
                             {
                                 StateId = 12,
-                                StateName = "Alabama"
+                                StateName = "Idaho"
                             },
                             new State
                             {
                                 StateId = 13,
-                                StateName = "Alabama"
+                                StateName = "Illinois"
                             },
                             new State
                             {
                                 StateId = 14,
-                                StateName = "Alabama"
+                                StateName = "Indiana"
                             },
                             new State
                             {
                                 StateId = 15,
-                                StateName = "Alabama"
+                                StateName = "Iowa"
                             },
                             new State
                             {
                                 StateId = 16,
-                                StateName = "Alabama"
+                                StateName = "Kentucky"
                             },
                             new State
                             {
                                 StateId = 17,
-                                StateName = "Alabama"
+                                StateName = "Louisiana"
                             },
                             new State
                             {
                                 StateId = 18,
-                                StateName = "Alabama"
+                                StateName = "Maine"
                             },
                             new State
                             {
                                 StateId = 19,
-                                StateName = "Alabama"
+                                StateName = "Maryland"
                             },
                             new State
                             {
                                 StateId = 20,
-                                StateName = "Alabama"
+                                StateName = "Massachusetts"
                             },
                             new State
                             {
                                 StateId = 21,
-                                StateName = "Alabama"
+                                StateName = "Michigan"
                             },
                             new State
                             {
                                 StateId = 22,
-                                StateName = "Alabama"
+                                StateName = "Minnesota"
                             },
                             new State
                             {
                                 StateId = 23,
-                                StateName = "Alabama"
+                                StateName = "Mississippi"
                             },
                             new State
                             {
                                 StateId = 24,
-                                StateName = "Alabama"
+                                StateName = "Missouri"
                             },
                             new State
                             {
                                 StateId = 25,
-                                StateName = "Alabama"
+                                StateName = "Montana"
                             },
                             new State
                             {
                                 StateId = 26,
-                                StateName = "Alabama"
+                                StateName = "Nebraska"
                             },
                             new State
                             {
                                 StateId = 27,
-                                StateName = "Alabama"
+                                StateName = "Nevada"
                             },
                             new State
                             {
                                 StateId = 28,
-                                StateName = "Alabama"
+                                StateName = "New Hampshire"
                             },
                             new State
                             {
                                 StateId = 29,
-                                StateName = "Alabama"
+                                StateName = "New Jersey"
                             },
                             new State
                             {
                                 StateId = 30,
-                                StateName = "Alabama"
+                                StateName = "New Mexico"
                             },
                             new State
                             {
                                 StateId = 31,
-                                StateName = "Alabama"
+                                StateName = "New York"
                             },
                             new State
                             {
                                 StateId = 32,
-                                StateName = "Alabama"
+                                StateName = "North Carolina"
                             },
                             new State
                             {
                                 StateId = 33,
-                                StateName = "Alabama"
+                                StateName = "North Dakota"
                             },
                             new State
                             {
                                 StateId = 34,
-                                StateName = "Alabama"
+                                StateName = "Ohio"
                             },
                             new State
                             {
                                 StateId = 35,
-                                StateName = "Alabama"
+                                StateName = "Oklahoma"
                             },
                             new State
                             {
                                 StateId = 36,
-                                StateName = "Alabama"
+                                StateName = "Oregon"
                             },
                             new State
                             {
                                 StateId = 37,
-                                StateName = "Alabama"
+                                StateName = "Pennsylvania"
                             },
                             new State
                             {
                                 StateId = 38,
-                                StateName = "Alabama"
+                                StateName = "Rhode Island"
                             },
                             new State
                             {
                                 StateId = 39,
-                                StateName = "Alabama"
+                                StateName = "South Carolina"
                             },
                             new State
                             {
                                 StateId = 40,
-                                StateName = "Alabama"
+                                StateName = "South Dakota"
                             },
                             new State
                             {
                                 StateId = 41,
-                                StateName = "Alabama"
+                                StateName = "Tennessee"
                             },
                             new State
                             {
                                 StateId = 42,
-                                StateName = "Alabama"
+                                StateName = "Texas"
                             },
                             new State
                             {
                                 StateId = 43,
-                                StateName = "Alabama"
+                                StateName = "Utah"
                             },
                             new State
                             {
                                 StateId = 44,
-                                StateName = "Alabama"
+                                StateName = "Vermont"
                             },
                             new State
                             {
                                 StateId = 45,
-                                StateName = "Alabama"
+                                StateName = "Virginia"
                             },
                             new State
                             {
                                 StateId = 46,
-                                StateName = "Alabama"
+                                StateName = "Washington"
                             },
                             new State
                             {
                                 StateId = 47,
-                                StateName = "Alabama"
+                                StateName = "West Virginia"
                             },
                             new State
                             {
                                 StateId = 48,
-                                StateName = "Alabama"
+                                StateName = "Wisconsin"
                             },
                             new State
                             {
                                 StateId = 49,
-                                StateName = "Alabama"
+                                StateName = "Wyoming"
                             },
                              new State
                             {
                                 StateId = 50,
-                                StateName = "Alabama"
+                                StateName = "Kansas"
                             }
                         );
             }
